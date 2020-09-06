8c185bf76c4afa98aa2d1213ef4d868e50490259
Ojaswa Sharma <ojaswa1942@gmail.com>
2020-09-07 04:40:45 +0530

    feat: add link tracing & highlighting

--- watch.go
+++ watch.go
@@ -2,28 +2,32 @@ package watch
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
+	"github.com/alecthomas/chroma/formatters/html"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
+	"regexp"
 	"runtime/debug"
-	"bytes"
-	"github.com/alecthomas/chroma/quick"
+	"strconv"
+	"strings"
 )
 
 func WatchMw(app http.Handler, dev bool) http.HandlerFunc {
-	// app.HandleFunc("/watchMw/debug", func (w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintln(w, "<h1>Hello!</h1>")
-	// })
-
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			err := recover()
 			if err != nil {
 				stackTrace := string(debug.Stack())
+				log.Println("----------WATCH: LOG START----------")
 				log.Printf("[WATCH] panic: %v\nFollowing is the stack trace: %s", err, stackTrace)
+				log.Println("----------WATCH: LOG END----------")
 
 				if !dev {
 					http.Error(w, "Something went wrong!", http.StatusInternalServerError)
@@ -31,14 +35,14 @@ func WatchMw(app http.Handler, dev bool) http.HandlerFunc {
 				}
 
 				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Fprintf(w, "<h1>panic: %v</h1><h2>Stack trace:</h2><pre>%s</pre>", err, stackTrace)
-
-				filePath := r.FormValue("path")
-				if e := sourceCodeHandler(w, filePath); e != nil {
-					http.Error(w, e.Error(), http.StatusInternalServerError)
-				}
+				fmt.Fprintf(w, "<h1>panic: %v</h1><h2>Stack trace:</h2><pre>%s</pre>", err, getLinkTrace(stackTrace))
 			}
 		}()
+
+		if path := r.URL.Path; strings.Contains(path, "/watch/debug") {
+			sourceCodeHandler(w, r)
+			return
+		}
 
 		nw := &customResponseWriter{ResponseWriter: w}
 		app.ServeHTTP(nw, r)
@@ -47,25 +51,82 @@ func WatchMw(app http.Handler, dev bool) http.HandlerFunc {
 	}
 }
 
-// Writes source code to a dst writer
-func sourceCodeHandler(dst io.Writer, filePath string) error {
-	// filePath := "/home/ojaswa/Projects/go-watch-middleware/example/main.go"
-	file, err := os.Open(filePath) 
+func sourceCodeHandler(w http.ResponseWriter, r *http.Request) {
+	filePath := r.FormValue("path")
+	lineStr := r.FormValue("line")
+	lineNumber, err := strconv.Atoi(lineStr)
 	if err != nil {
-		return err
+		lineNumber = -1
+	}
+
+	fileContent, err := getFileContent(filePath)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	writeSource, err := getFormattedSource(fileContent, lineNumber)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	err = writeSource(w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func getFileContent(filePath string) (string, error) {
+	file, err := os.Open(strings.Trim(filePath, "\""))
+	if err != nil {
+		return "", err
 	}
 
 	fileBytes := bytes.NewBuffer(nil)
 	if _, err = io.Copy(fileBytes, file); err != nil {
-		return err
+		return "", err
 	}
 
-	err = quick.Highlight(dst, fileBytes.String(), "go", "html", "monokai")
-	if err != nil {
-		return err
+	return fileBytes.String(), nil
+}
+
+func getFormattedSource(content string, lineNumber int) (func(io.Writer) error, error) {
+	var highlightLine [][2]int
+	if lineNumber > 0 {
+		highlightLine = append(highlightLine, [2]int{lineNumber, lineNumber})
 	}
-	
-	return nil
+
+	lexer := lexers.Get("go")
+	iterator, err := lexer.Tokenise(nil, content)
+	if err != nil {
+		return nil, err
+	}
+
+	style := styles.Get("monokailight")
+	if style == nil {
+		style = styles.Fallback
+	}
+	formatter := html.New(html.TabWidth(2), html.WithLineNumbers(true), html.LineNumbersInTable(true), html.HighlightLines(highlightLine))
+
+	return func(w io.Writer) error {
+		err := formatter.Format(w, style, iterator)
+		if err != nil {
+			return err
+		}
+		return nil
+	}, nil
+}
+
+func getLinkTrace(stackTrace string) string {
+	re := regexp.MustCompile(`\t.*:\d*`)
+	matches := re.ReplaceAllStringFunc(stackTrace, func(match string) string {
+		split := strings.Split(match, ":")
+		path, lineNum := strings.Trim(split[0], "\t "), split[1]
+		return fmt.Sprintf("> <a target=\"_blank\" href=/watch/debug/?line=%s&path=%s>%s:%s</a>", lineNum, url.PathEscape(path), path, lineNum)
+	})
+
+	return matches
 }
 
 type customResponseWriter struct {
